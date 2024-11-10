9e421e90b085d12321e38b324be6a2ec84065e00
tgkhanhDev <148732710+tgkhanhDev@users.noreply.github.com>
2024-11-10 15:52:24 +0700

    feat: add food entity, config swagger-ui underpath: /api-baemin

--- baemin_be/src/app.module.ts
+++ baemin_be/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { FoodModule } from './food/food.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, FoodModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- baemin_be/src/main.ts
+++ baemin_be/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+  .setTitle("Baemin Application")
+  .setDescription("By tgkhanhDev")
+  .setVersion("1.0")
+  .addTag('cat')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-baemin', app, document);
+  
+  await app.listen(process.env.PORT ?? 8080);
 }
+
 bootstrap();
+//1yarn prisma init
+//2keo vao trong src:
+//  yarn db pull --schema src/prisma/<schema-file-name>
+//3 yarn prisma generate --schema src/prisma/<schema-file-name>
