0574acb5a7d9383b2341870ec4741962d8ab9f7a
trangiangkhanh175 <trangiangkhanh175.work@gmail.com>
2024-11-27 19:57:50 +0700

    feat: basic java gateway

---
+++ baemin-be/java-gateway/src/main/java/api_gateway/JavaGatewayApplication.java
@@ -0,0 +1,13 @@
+package api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGatewayApplication.class, args);
+	}
+
+}
---
+++ baemin-be/java-gateway/src/main/java/api_gateway/consumer/RabbitMQConsumer.java
@@ -0,0 +1,17 @@
+package api_gateway.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumer.class);
+
+    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
+    public void comsume(String message) {
+        LOGGER.info(String.format("Message received -> %s", message));
+    }
+
+}
