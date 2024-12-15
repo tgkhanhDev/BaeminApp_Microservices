cf0b180e68edd73f8161512706e11cf17ed6c098
trangiangkhanh175 <trangiangkhanh175.work@gmail.com>
2024-12-15 16:12:10 +0700

    feat: rm comment, db

--- baemin-be/nest-food-services/src/main.ts
+++ baemin-be/nest-food-services/src/main.ts
@@ -18,30 +18,5 @@ async function bootstrap() {
     }
   });
   await app.listen();
-
-  //*=================
-  // const app = await NestFactory.create(AppModule);
-  // app.enableCors();
-  // app.use((err, req, res, next) => {
-  //   const statusCode = err.status || 500;
-  //   const message = err.message || "Internal Server Error";
-  //   res.status(statusCode).json({
-  //     statusCode,
-  //     message
-  //   });
-  // });
-
-  // const config = new DocumentBuilder()
-  //   .setTitle("Baemin Application")
-  //   .setDescription("By tgkhanhDev")
-  //   .setVersion("1.0")
-  //   .build();
-
-  // const document = SwaggerModule.createDocument(app, config);
-
-  // SwaggerModule.setup('api-baemin', app, document);
-  
-  // await app.listen(8080);
-  //*=================
 }
 bootstrap();
